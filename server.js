c23791d463a693407a334b01edb8df834f332c2e
miguelangelabou <miguelangelabou@gmail.com>
2025-01-25 01:15:29 -0400

    Subiendo archivos

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Rutas para cada idioma
+app.get('/es', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/en', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/ar', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/ch', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Ruta por defecto
+app.get('/', (req, res) => {
+    res.redirect('/en');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./styles/**/*.css", "./scripts/**/*.js"], // Asegúrate de incluir los archivos CSS y JS relevantes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
