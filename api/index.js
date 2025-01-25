a1dee582e0f949c572a6e494f6d3f752c4868d4a
Miguelangel <miguelangelabou@gmail.com>
2025-01-25 12:28:22 -0400

    Subiendo modificaciones del serverless

--- api/index.js
+++ api/index.js
@@ -1,16 +1,23 @@
+const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
-module.exports = (req, res) => {
+const app = express();
+
+// Servir archivos estáticos desde la carpeta public
+app.use('/styles', express.static(path.join(__dirname, '../public/styles')));
+app.use('/scripts', express.static(path.join(__dirname, '../public/scripts')));
+
+// Ruta para servir el archivo HTML
+app.get('/', (req, res) => {
     const filePath = path.join(__dirname, '../index.html');
+    res.sendFile(filePath);
+});
 
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error al cargar el archivo HTML');
-            return;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-    });
-};
+// Manejar otras rutas si es necesario
+app.get('/en', (req, res) => {
+    const filePath = path.join(__dirname, '../index.html');
+    res.sendFile(filePath);
+});
+
+// Exportar la aplicación
+module.exports = app;
