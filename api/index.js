9235410e8df29e4c6dd4a3813de77c836871d461
Miguelangel <miguelangelabou@gmail.com>
2025-01-26 01:58:29 -0400

    Subiendo cambios a carrusel

--- api/index.js
+++ api/index.js
@@ -3,22 +3,33 @@ const path = require('path');
 
 const app = express();
 
-// Servir archivos estáticos desde la carpeta public
 app.use('/styles', express.static(path.join(__dirname, '../public/styles')));
 app.use('/scripts', express.static(path.join(__dirname, '../public/scripts')));
 app.use('/assets', express.static(path.join(__dirname, '../public/assets')));
 
-// Ruta para servir el archivo HTML
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, '../index.html');
     res.sendFile(filePath);
 });
 
-// Manejar otras rutas si es necesario
 app.get('/en', (req, res) => {
     const filePath = path.join(__dirname, '../index.html');
     res.sendFile(filePath);
 });
 
-// Exportar la aplicación
+app.get('/es', (req, res) => {
+    const filePath = path.join(__dirname, '../index.html');
+    res.sendFile(filePath);
+});
+
+app.get('/ar', (req, res) => {
+    const filePath = path.join(__dirname, '../index.html');
+    res.sendFile(filePath);
+});
+
+app.get('/ch', (req, res) => {
+    const filePath = path.join(__dirname, '../index.html');
+    res.sendFile(filePath);
+});
+
 module.exports = app;
