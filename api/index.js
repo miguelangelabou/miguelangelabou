31cadf9cd618ee38a4ee7f5ced1f8964835a1a2a
Miguelangel <miguelangelabou@gmail.com>
2025-02-22 17:53:01 -0400

    Cambios

--- api/index.js
+++ api/index.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const sslify = require('express-sslify');
-const cors = require('cors');
 
 const app = express();
-
-app.use(morgan('combined'));
-app.use(helmet());
-app.use(cors());
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100
-});
-
-app.use(limiter);
-app.use(sslify.HTTPS({ trustProtoHeader: true }));
-
 
 app.use('/styles', express.static(path.join(__dirname, '../public/styles')));
 app.use('/scripts', express.static(path.join(__dirname, '../public/scripts')));
