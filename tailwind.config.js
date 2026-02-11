5f2283ffddf49059018474d115851dd0e823af87
Miguelangel Abou <miguelangelabou@gmail.com>
2026-02-11 19:42:10 -0400

    .

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./public/styles/**/*.css", "./public/scripts/**/*.js", "./**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
