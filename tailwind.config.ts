faec0c3416470db8570764584140c7cac9ae2b5e
Miguelangel Abou <miguelangelabou@gmail.com>
2026-02-11 20:50:05 -0400

    .

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: { extend: {} },
+  plugins: [
+    require('tailwindcss-animate'),
+  ],
+} satisfies Config
